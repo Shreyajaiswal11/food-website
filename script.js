3c98454d6e8006ee0a74ef8d51f0a8517287d1e9
Shreya <shreyajaiswal1102@gmail.com>
2022-01-17 02:42:11 +0530

    food website

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelector('.menu').addEventListener('click',()=>
+{
+    document.querySelectorAll('.target').forEach((item) =>{
+        item.classList.toggle('change')
+    })
+})
+const icons= document.querySelectorAll('.section1-icons i');
+let i=1;
+setInterval(()=>{
+    i++;
+const icon=document.querySelector('.section1-icons .change')
+icon.classList.remove('change');
+if(i>icons.length){
+    icons[0].classList.add('change')
+    i=1;
+}
+else
+icon.nextElementSibling.classList.add('change');
+},2000)
